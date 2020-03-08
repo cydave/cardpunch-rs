3e394b556a2e72fdcd30f611a951a49225c5922c
dave <dave@cyllective.com>
2020-03-08 16:20:57 +0100

    update

--- src/main.rs
+++ src/main.rs
@@ -2,7 +2,7 @@ use std::collections::HashMap;
 use std::fs::File;
 use std::string::String;
 
-use serde::{Deserialize};
+use serde::Deserialize;
 
 type Column = [i32; 12];
 type Row = Vec<char>;
@@ -11,9 +11,8 @@ type Row = Vec<char>;
 struct CharsetFile {
     on_char: char,
     off_char: char,
-    charmap: HashMap<char, Column>
+    charmap: HashMap<char, Column>,
 }
-
 
 impl CharsetFile {
     fn load(path: &str) -> Self {
@@ -22,21 +21,28 @@ impl CharsetFile {
     }
 }
 
-#[derive(Debug, Deserialize)]
+#[derive(Debug)]
 struct Charset {
     on_char: char,
     off_char: char,
     enc_map: HashMap<char, Column>,
-    dec_map: HashMap<Column, char>
+    dec_map: HashMap<Column, char>,
 }
 
 impl Charset {
-
     fn from_file(path: &str) -> Self {
         let charset_file = CharsetFile::load(path);
         let enc_map = charset_file.charmap.to_owned();
-        let dec_map = enc_map.iter().map(|(k, v)| (v.to_owned(), k.to_owned())).collect();
-        Self { on_char: charset_file.on_char, off_char: charset_file.off_char, enc_map: charset_file.charmap.to_owned(), dec_map }
+        let dec_map = enc_map
+            .iter()
+            .map(|(k, v)| (v.to_owned(), k.to_owned()))
+            .collect();
+        Self {
+            on_char: charset_file.on_char,
+            off_char: charset_file.off_char,
+            enc_map,
+            dec_map,
+        }
     }
 
     fn encode(&self, k: char) -> Column {
@@ -62,7 +68,10 @@ struct Card {
 
 impl Card {
     fn with_charset(charset: Charset) -> Self {
-        Card { charset: charset, columns: Vec::new()  }
+        Card {
+            charset,
+            columns: Vec::new(),
+        }
     }
 
     fn punch_char(&mut self, c: char) {
@@ -77,7 +86,10 @@ impl Card {
     }
 
     fn read(&self) -> String {
-        self.columns.iter().map(|c| self.charset.decode(*c)).collect()
+        self.columns
+            .iter()
+            .map(|c| self.charset.decode(*c))
+            .collect()
     }
 
     fn rows(&self) -> Vec<Row> {
@@ -88,7 +100,7 @@ impl Card {
                 let c: char = match column[i] {
                     1 => self.charset.on_char,
                     0 => self.charset.off_char,
-                    _ => self.charset.on_char
+                    _ => self.charset.on_char,
                 };
                 row.push(c);
             }
@@ -99,13 +111,13 @@ impl Card {
 
     fn print(&self) {
         let rows = self.rows();
-        let card_width = self.columns.len()+1;
+        let card_width = self.columns.len() + 1;
         println!("    {}", "_".repeat(card_width));
         println!("   /{}", self.read());
         println!("12/ {}", rows[0].iter().collect::<String>());
         println!("11|  {}", rows[1].iter().collect::<String>());
-        for (index, row) in rows[2..].into_iter().enumerate() {
-            println!(" {}|  {}", index.to_string(), row.into_iter().collect::<String>());
+        for (index, row) in rows[2..].iter().enumerate() {
+            println!(" {}|  {}", index, row.iter().collect::<String>());
         }
         println!("  |__{}", "_".repeat(card_width));
     }
